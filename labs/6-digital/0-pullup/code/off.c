bf4fa0c75259ac10a998f2d4cf7ef67fd8a4eb80
Garand Tyson <gttyson@stanford.edu>
2020-04-30 21:54:50 -0500

    Update

--- labs/6-digital/0-pullup/code/off.c
+++ labs/6-digital/0-pullup/code/off.c
@@ -1,37 +1,31 @@
 // trivial program to reset the pins to off.
 #include "rpi.h"
 
-#define GPPUD 0x20200094
-#define GPPUDCLK0 0x20200098
-#define GPPUDCLK1 0x2020009c
+volatile void *GPPUD = (volatile void *)0x20200094;
+volatile void *GPPUDCLK0 = (volatile void *)0x20200098;
+volatile void *GPPUDCLK1 = (volatile void *)0x2020009c;
 #define DELAY 150
 
 void gpio_set_pullup(unsigned pin) { 
-    *(volatile unsigned *)(GPPUD) = 0x10; // Enable pull-up
+    put32(GPPUD, 0x10); // Enable pull-up
     delay_us(150);
-    *(volatile unsigned *)(GPPUDCLK0) = 1 << pin;
-    // delay_us(150);
-    // *(volatile unsigned *)(GPPUD) = 0;
+    put32(GPPUDCLK0, 1 << pin);
     delay_us(150);
-    *(volatile unsigned *)(GPPUDCLK0) = 0;
+    put32(GPPUDCLK0, 1 << pin);
 }
 void gpio_set_pulldown(unsigned pin) {
-    *(volatile unsigned *)(GPPUD) = 0x01; // Enable pull-down
+    put32(GPPUD, 0x01); // Enable pull-up
     delay_us(150);
-    *(volatile unsigned *)(GPPUDCLK0) = 1 << pin;
-    // delay_us(150);
-    // *(volatile unsigned *)(GPPUD) = 0;
+    put32(GPPUDCLK0, 1 << pin);
     delay_us(150);
-    *(volatile unsigned *)(GPPUDCLK0) = 0;
+    put32(GPPUDCLK0, 1 << pin);
  }
 void gpio_pud_off(unsigned pin) { 
-    *(volatile unsigned *)(GPPUD) = 0; // Disable pull-down
+    put32(GPPUD, 0x00); // Enable pull-up
     delay_us(150);
-    *(volatile unsigned *)(GPPUDCLK0) = 1 << pin;
-    // delay_us(150);
-    // *(volatile unsigned *)(GPPUD) = 0;
+    put32(GPPUDCLK0, 1 << pin);
     delay_us(150);
-    *(volatile unsigned *)(GPPUDCLK0) = 0;
+    put32(GPPUDCLK0, 1 << pin);
  }
 
 void notmain(void) {
--- labs/6-digital/0-pullup/code/on.c
+++ labs/6-digital/0-pullup/code/on.c
@@ -1,37 +1,31 @@
 // trivial program to set pin 21 to a pullup, 20 to a pulldown.
 #include "rpi.h"
 
-#define GPPUD 0x20200094
-#define GPPUDCLK0 0x20200098
-#define GPPUDCLK1 0x2020009c
+volatile void *GPPUD = (volatile void *)0x20200094;
+volatile void *GPPUDCLK0 = (volatile void *)0x20200098;
+volatile void *GPPUDCLK1 = (volatile void *)0x2020009c;
 #define DELAY 150
 
 void gpio_set_pullup(unsigned pin) { 
-    *(volatile unsigned *)(GPPUD) = 0x10; // Enable pull-up
+    put32(GPPUD, 0x10); // Enable pull-up
     delay_us(150);
-    *(volatile unsigned *)(GPPUDCLK0) = 1 << pin;
-    // delay_us(150);
-    // *(volatile unsigned *)(GPPUD) = 0;
+    put32(GPPUDCLK0, 1 << pin);
     delay_us(150);
-    *(volatile unsigned *)(GPPUDCLK0) = 1;
+    put32(GPPUDCLK0, 1 << pin);
 }
 void gpio_set_pulldown(unsigned pin) {
-    *(volatile unsigned *)(GPPUD) = 0x01; // Enable pull-down
+    put32(GPPUD, 0x01); // Enable pull-up
     delay_us(150);
-    *(volatile unsigned *)(GPPUDCLK0) = 1 << pin;
-    // delay_us(150);
-    // *(volatile unsigned *)(GPPUD) = 0;
+    put32(GPPUDCLK0, 1 << pin);
     delay_us(150);
-    *(volatile unsigned *)(GPPUDCLK0) = 1;
+    put32(GPPUDCLK0, 1 << pin);
  }
 void gpio_pud_off(unsigned pin) { 
-    *(volatile unsigned *)(GPPUD) = 0; // Disable pull-down
+    put32(GPPUD, 0x00); // Enable pull-up
     delay_us(150);
-    *(volatile unsigned *)(GPPUDCLK0) = 1 << pin;
-    // delay_us(150);
-    // *(volatile unsigned *)(GPPUD) = 0;
+    put32(GPPUDCLK0, 1 << pin);
     delay_us(150);
-    *(volatile unsigned *)(GPPUDCLK0) = 1;
+    put32(GPPUDCLK0, 1 << pin);
  }
 
 void notmain(void) {
@@ -44,7 +38,7 @@ void notmain(void) {
     gpio_set_pullup(up);
     printk("pullup=%d, this is sticky: touch red lead to this pin and black to ground: should see a ~3V reading on multimeter\n", up);
 
-    gpio_set_input(up);
+    gpio_set_input(down);
     gpio_set_pulldown(down);
     printk("pulldown=%d, this is sticky: touch black lead to this pin, and red to 3v: should see a ~3V reading on multimeter\n", down);
 
