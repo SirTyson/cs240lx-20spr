8f332db862de48bec6f1ea20fc0842af31954a85
Garand Tyson <gttyson@stanford.edu>
2020-05-05 20:46:39 -0500

    Finally fixed pulldown

--- labs/6-fake/code/gpio-pud.c
+++ labs/6-fake/code/gpio-pud.c
@@ -1,6 +1,36 @@
 // put your pull-up/pull-down implementations here.
 #include "rpi.h"
 
-void gpio_set_pullup(unsigned pin) { unimplemented(); }
-void gpio_set_pulldown(unsigned pin) { unimplemented(); }
-void gpio_pud_off(unsigned pin) { unimplemented(); }
+#define GPPUD       0x20200094
+#define GPPUDCLK0   0x20200098
+#define DELAY       150
+
+void 
+gpio_set_pullup (unsigned pin) 
+{ 
+    put32 ((volatile void *) GPPUD, 0b10);
+    delay_us (DELAY);
+    put32 ((volatile void *) GPPUDCLK0, 1 << pin);
+    delay_us (DELAY);
+    put32 ((volatile void *) GPPUDCLK0, 0);
+}
+
+void 
+gpio_set_pulldown (unsigned pin) 
+{ 
+    put32 ((volatile void *) GPPUD, 0b01);
+    delay_us (DELAY);
+    put32 ((volatile void *) GPPUDCLK0, 1 << pin);
+    delay_us (DELAY);
+    put32 ((volatile void *) GPPUDCLK0, 0);
+}
+
+void
+gpio_pud_off (unsigned pin) 
+{ 
+    put32 ((volatile void *) GPPUD, 0);
+    delay_us (DELAY);
+    put32 ((volatile void *) GPPUDCLK0, 1 << pin);
+    delay_us (DELAY);
+    put32 ((volatile void *) GPPUDCLK0, 0);
+}
